cfb62ac264b237e0d03ec6cf532c91af5f72088b
Joe Estrem <estremjoe@gmail.com>
2025-08-04 22:53:08 +0900

    Initial commit

---
+++ src/components/Kohls.js
@@ -0,0 +1,15 @@
+import "./Kohls.css";
+
+import React from 'react';
+
+function Kohls() {
+  return (
+  <div>
+    <h1>Kohl's</h1>
+    <p>Kohl’s is one of the leading American retailers, dedicated to supporting families through life’s everyday moments.</p>
+    <p>Social media, Print media</p>
+  </div>
+  );
+}
+
+export default Kohls;
