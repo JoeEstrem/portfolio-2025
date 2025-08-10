da8286b6188f7dbc8096c825d793834505b4961e
Joe Estrem <estremjoe@gmail.com>
2025-08-10 13:40:54 +0900

    made skeleton of show page

--- src/components/Kohls.js
+++ src/components/Kohls.js
@@ -5,12 +5,79 @@ import React from 'react';
 function Kohls() {
   return (
       <div class="grid">
-        <h1>Kohl's</h1>
+
+        <div class="col-12">
+          <h1>Kohl's</h1>
+        </div>
+
         <div class="landing">
-          <div class="landing-text">
+          <div class="white-text-box">
             <p>Kohl’s is one of the leading American retailers, dedicated to supporting families through life’s everyday moments.</p>
-            <p>Social media, Print media</p>
+            <p class="p-note">Social media, Print media</p>
           </div>
+        </div>
+
+        <div class="text-block">
+          <div class="col-4">
+            <h2>Social Media</h2>
+          </div>
+          <div class="col-4">
+            <p>I started with creative ideation, followed by collaborating with a photographer to capture the necessary visuals. I then handled final editing and compiled the content into completed assets.</p>
+          </div>
+        </div>
+
+        <div class="col-12">
+          <h3>New Gifts at Every Turn</h3>
+          <p class="p-note">Facebook carousel</p>
+        </div>
+        <div class="img-square-4"></div>
+        <div class="img-square-4"></div>
+        <div class="img-square-4"></div>
+        <div class="img-square-4"></div>
+
+        <div class="col-12">
+          <h3>Savannah Bee Company</h3>
+          <p class="p-note">Instagram story</p>
+        </div>
+        <div class="img-square-4"></div>
+        <div class="img-square-4"></div>
+        <div class="img-square-4"></div>
+        <div class="img-square-4"></div>
+
+        <div class="col-12">
+          <h3>Prodcut Highlights</h3>
+          <p class="p-note">Pinterest Pins</p>
+        </div>
+        <div class="img-rectangle-2"></div>
+        <div class="img-rectangle-2"></div>
+
+        <div class="text-block">
+          <div class="col-4">
+            <h2>Print Media</h2>
+          </div>
+          <div class="col-4">
+            <p>I started with creative ideation, followed by collaborating with a photographer to capture the necessary visuals. I then handled final editing and compiled the content into completed assets.</p>
+          </div>
+        </div>
+
+        <div class="col-12">
+          <h3>Welcome to the Neighborhood</h3>
+          <p class="p-note">Home mailer</p>
+        </div>
+        <div class="img-rectangle-2-hor"></div>
+        <div class="img-rectangle-2-hor"></div>
+
+        <div class="col-12">
+          <h3>Home Sweet (New) Home</h3>
+          <p class="p-note">Home mailer</p>
+        </div>
+        <div class="img-rectangle-2-hor"></div>
+        <div class="img-rectangle-2-hor"></div>
+
+
+
+        <div class="footer">
+          <p>This is the footer</p>
         </div>
       </div>
   );
