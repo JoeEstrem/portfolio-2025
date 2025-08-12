582302d825d86ffb88e26a662624bfd0cd51edba
Joe Estrem <estremjoe@gmail.com>
2025-08-12 20:17:48 +0900

    added auto scroll to top when selecting a new nav item

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Kollab from './components/Kollab';
 import PlansTonight from './components/PlansTonight';
 import Footer from './components/Footer';
 
+import ScrollToTop from "./components/ScrollToTop";
+
 function App() {
   return (
     <Router>
@@ -14,13 +16,13 @@ function App() {
       <div className="main-page-container">
         <SideNav />
         <div className="display-container">
+          <ScrollToTop />
           <Routes>
             <Route path="/Kohls" element={<Kohls />} />
             <Route path="/HousingJapan" element={<HousingJapan />} />
             <Route path="/Kollab" element={<Kollab />} />
             <Route path="/PlansTonight" element={<PlansTonight />} />
           </Routes>
-
           <div>
            <Footer />
           </div>
